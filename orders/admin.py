3b0c572f48682e88e0d238ddbbf8d3fb66ddb9a9
AmrKallab <amr.qallab556@gmail.com>
2026-06-06 12:58:09 +0300

    fix models and add admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "status",
+        "total_price",
+        "created_at",
+    ]
+
+    list_filter = [
+        "status",
+        "created_at",
+    ]
+
+    search_fields = [
+        "user__username",
+        "user__email",
+    ]
+
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "order",
+        "product_name",
+        "price",
+        "quantity",
+    ]
