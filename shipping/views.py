e9932e43a5a87bd2f9a5dfa42c20b7e46d6754d2
AmrKallab <amr.qallab556@gmail.com>
2026-09-13 17:16:03 +0300

    add shipping address api

--- shipping/views.py
+++ shipping/views.py
@@ -29,4 +29,44 @@ class ShippingAddressAPIView(APIView):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+class ShippingAddressDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self,pk,request) :
+
+        shippingaddress = get_object_or_404(ShippingAddress,pk=pk,user=request.user) 
+
+        return shippingaddress
+
+    def get(self,request,pk) :
+
+        shippingaddress = self.get_object(pk,request) 
+
+        serializer = ShippingAddressSerializer(shippingaddress)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def put(self,request,pk) :
+
+        shippingaddress = self.get_object(pk,request) 
+
+        serializer = ShippingAddressSerializer(shippingaddress,data=request.data) 
+        if serializer.is_valid() :
+            serializer.save() 
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self,request,pk) :
+        
+        shippingaddress = self.get_object(pk,request) 
+
+        serializer = ShippingAddressSerializer(shippingaddress,data=request.data) 
+        if serializer.is_valid() :
+            serializer.save() 
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
